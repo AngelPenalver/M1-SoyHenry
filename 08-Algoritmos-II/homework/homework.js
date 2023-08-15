2d7c8ca76aa25f9c2f58937c5532f1737bfca6d7
unknown <apenalver4@gmail.com>
2023-08-15 09:18:05 -0400

    M1-BootCamp-SoyHenry

--- 08-Algoritmos-II/homework/homework.js
+++ 08-Algoritmos-II/homework/homework.js
@@ -37,6 +37,30 @@ function mergeSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+  function mergeSort(array) { // Implementar el método conocido como mergeSort para ordenar de menor a mayor // el array recibido como parámetro // Devolver el array ordenado resultante // Tu código: 
+    if(array.length <= 1) return array; 
+    let mid = Math.floor(array.length / 2); 
+    let left = []; let right = []; // Copiar la primera mitad del array en left 
+    for(let i = 0; i < mid; i++) { left.push(array[i]); } // Copiar la segunda mitad del array en right 
+    for(let i = mid; i < array.length; i++) { right.push(array[i]); } // Ordenar recursivamente las mitades 
+    left = mergeSort(left); right = mergeSort(right); 
+    let resultado = []; // Usar variables para guardar los índices de los arrays 
+    let i = 0; // Índice de left 
+    let j = 0; // Índice de right 
+    while(i < left.length && j < right.length)
+    { if(left[i] < right[j]){ resultado.push(left[i]); i++; // Incrementar el índice de left 
+} else { resultado.push(right[j]); j++; // Incrementar el índice de right 
+} } // Concatenar los elementos restantes de left y right al final del resultado
+ for(let k = i; k < left.length; k++) 
+ { resultado.push(left[k]); 
+} 
+for(let k = j; k < right.length; k++) { 
+    resultado.push(right[k]); } 
+    return resultado;
+
+}
+console.log(mergeSort([5,9,2,6,1,4,8,3]));
+
 
   //okey probemos ahora
 }
